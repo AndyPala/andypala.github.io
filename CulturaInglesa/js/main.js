576bdf0aa30a0678321a9c3c1c4d52ddbea176bb
AndyPala <andres_go5@hotmail.com>
2015-04-26 23:01:49 -0500

    uk edu pt3
    slider oxford

--- CulturaInglesa/js/main.js
+++ CulturaInglesa/js/main.js
@@ -1,6 +1,7 @@
 $("document").ready(function(){
 	$('.sidebar').first().sidebar('attach events', '.button');
 	$('.toggle.button').removeClass('disabled');
-	$('.menu .item').tab()
-;
+	$('.menu .item').tab();
+	var options = { $AutoPlay: true };
+	var jssor_slider1 = new $JssorSlider$('slider2_container', options);
 });
